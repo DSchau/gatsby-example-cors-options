d3faa329bb902fbf027167d954767e8550c1f8ea
Dustin Schau <dustinschau@gmail.com>
2019-08-20 14:17:01 -0700

    chore: noop change

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `content`,
-        path: `${__dirname}/content`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
